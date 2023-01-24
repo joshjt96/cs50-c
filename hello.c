53bef010651af1665909ae562e0b7121f0aec02d
Josh Talley <jtdev84@gmail.com>
2023-01-24 09:52:53 +0000

    Add file

---
+++ hello.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(voic)
+{
+    string answer = get_string("What is your name? ");
+    printf("hello, %s", answer);
+
+    int x = get_in("What's x? ");
+    int y = get_int("What's y? ");
+
+    if (x < y)
+{ 
+    printf("X is less than Y/n")
+}
+else if (x > y)
+{
+    printf("X is greater than Y/n")
+}
+else
+{
+    printf("X is equal to Y/n")
+}
+}
+
+int counter = 1
+counter = counter++
+
+int i = 0
+while (i <= 3)
+{
+    printf("meow\n");
+    i++;
+}
+
+// For loop:
+for (int i = 0; i < 3; i++)
+{
+     printf("meow\n");
+}
+
