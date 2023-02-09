1d5f3041ad3a00358a82787096b20cd0dd4fe087
Josh Talley <jtdev84@gmail.com>
2023-02-09 11:11:59 +0000

    Add count_words function

--- readability.c
+++ readability.c
@@ -3,20 +3,35 @@
 #include <string.h>
 
 int count_letters(string input);
+int count_words(string text);
 
 int main(void)
 {
     string userInput = get_string("Enter a sentence: ");
     string letters = " letters";
+    string words = " words";
     printf("%s\n", userInput);
     int userInputLength = count_letters(userInput);
     printf("%i", userInputLength);
     printf("%s\n", letters);
-
+    int wordsLength = count_words(userInput);
+    printf("%i", wordsLength);
+    printf("%s\n", words);
 }
 
 int count_letters(string input)
 {
     int textLength = strlen(input);
     return textLength;
+}
+
+int count_words(string text)
+{
+    int amountOfWords = 1;
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == ' ' && text[i+1] != ' ')
+        amountOfWords++;
+    }
+    return amountOfWords;
 }
