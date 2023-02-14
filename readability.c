985d06c27964bf3aecda4f07610d11258255bcc9
Josh Talley <jtdev84@gmail.com>
2023-02-14 09:17:55 +0000

    Add count_sentences function

--- readability.c
+++ readability.c
@@ -6,38 +6,52 @@
 
 int count_letters(string input);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     string userInput = get_string("Enter a sentence: ");
-    string letters = " letters";
-    string letter = " letter";
-    string words = " words";
-    string word = " word";
-    int userInputLength = count_letters(userInput);
-    int wordsLength = count_words(userInput);
-    
-    printf("%s\n", userInput);
-    printf("%i", userInputLength);
+    // string letters = " letters";
+    // string letter = " letter";
+    // string words = " words";
+    // string word = " word";
+    // string sentence = " sentence";
+    // string sentences = " sentences";
 
-    if (userInputLength == 1)
+    int lettersLength = count_letters(userInput);
+    int wordsLength = count_words(userInput);
+    int sentenceLength = count_sentences(userInput);
+
+    printf("%s\n", userInput);
+
+    printf("%i", lettersLength);
+    if (lettersLength == 1)
     {
-        printf("%s\n", letter);
+        printf("%s\n", " letter");
     }
     else
     {
-        printf("%s\n", letters);
+        printf("%s\n", " letters");
     }
 
     printf("%i", wordsLength);
-
     if (wordsLength == 1)
     {
-        printf("%s\n", word);
+        printf("%s\n", " word");
     }
     else
     {
-        printf("%s\n", words);
+        printf("%s\n", " words");
+    }
+
+    printf("%i", sentenceLength);
+    if (sentenceLength == 1)
+    {
+        printf("%s\n", " sentence");
+    }
+    else
+    {
+        printf("%s\n", " sentences");
     }
 }
 
@@ -68,4 +82,15 @@ int count_words(string text)
         amountOfWords++;
     }
     return amountOfWords;
+}
+
+int count_sentences(string text)
+{
+    int numberOfSentences = 0;
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        numberOfSentences++;
+    }
+    return numberOfSentences;
 }
